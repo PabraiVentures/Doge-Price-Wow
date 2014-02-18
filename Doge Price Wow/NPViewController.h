4790f08e444ee68bd7a36000a95fa77e4381c10f
Nathan Pabrai <nathanpabrai@gmail.com>
2014-02-17 22:43:14 -0500

    with icons

--- Doge Price Wow/NPViewController.h
+++ Doge Price Wow/NPViewController.h
@@ -9,6 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface NPViewController : UIViewController <NSURLConnectionDelegate>
+{
+  
+  bool forward;
+}
+@property (weak, nonatomic) IBOutlet UISegmentedControl *BitDollSegControl;
 @property (nonatomic,strong)NSMutableData* returnData;
 @property (nonatomic,strong)  NSURLConnection* currentConnection;
+@property (weak, nonatomic) IBOutlet UITextField *amountField;
+@property (weak, nonatomic) IBOutlet UITextView *rateField;
+@property (weak, nonatomic) IBOutlet UILabel *typeLabel;
+- (IBAction)convertAction:(id)sender;
+- (IBAction)directionChangeAction:(id)sender;
+- (IBAction)currChanged:(id)sender;
+
 @end
--- Doge Price Wow/NPViewController.m
+++ Doge Price Wow/NPViewController.m
@@ -16,17 +16,34 @@
 {
   [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-  NSString *restCallString = [NSString stringWithFormat:@"https://www.dogeapi.com/wow/?a=get_current_price" ];
+  
+  UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                 initWithTarget:self
+                                 action:@selector(dismissKeyboard)];
+  
+  [self.view addGestureRecognizer:tap];
+  
+  NSString *restCallString;
+  self->forward=true;
+  if (self.BitDollSegControl.selectedSegmentIndex == 0){
+    restCallString = [NSString stringWithFormat:@"https://www.dogeapi.com/wow/?a=get_current_price" ];
+  }
+  
+  else{
+    
+    restCallString = [NSString stringWithFormat:@"https://www.dogeapi.com/wow/?a=get_current_price&convert_to=BTC" ];
+
+  }
+  
   NSURL *restUrl=[NSURL URLWithString:restCallString];
   NSURLRequest *restRequest=[NSURLRequest requestWithURL:restUrl];
   
   
-  /* if( currentConnection)
+  if(self.currentConnection)
    {
-   [currentConnection cancel];
-   currentConnection = nil;
-   //  self.apiReturnXMLData = nil;
-   }*/
+     [self.currentConnection cancel];
+     self.currentConnection = nil;
+     self.returnData=nil;   }
   self.currentConnection= [[NSURLConnection alloc]initWithRequest:restRequest delegate:self];
   self.returnData=[NSMutableData data];
   
@@ -52,6 +69,34 @@
   
   NSString *returnString = [[NSString alloc] initWithData:self.returnData encoding:NSUTF8StringEncoding];
   NSLog(@"Results: %@" ,returnString);
+  
+  //DOLLAR MODE
+  if (self.BitDollSegControl.selectedSegmentIndex == 0){
+    if (self->forward){
+      
+    [  self.rateField setText:[NSString stringWithFormat:@"$%.2f is worth:\n%f dogecoin", self.amountField.text.floatValue, self.amountField.text.floatValue/returnString.floatValue]];
+      
+    }
+    else{
+      [  self.rateField setText:[NSString stringWithFormat:@"%.f dogecoin is worth:\n$%.2f ",self.amountField.text.floatValue, self.amountField.text.floatValue*returnString.floatValue]];
+      
+    }
+    
+  }
+  //BITCOIN MODE
+  else{
+    if (self->forward){
+      
+      [  self.rateField setText:[NSString stringWithFormat:@"%.6fBTC is worth:\n%f dogecoin", self.amountField.text.floatValue, self.amountField.text.floatValue/returnString.floatValue]];
+      
+    }
+    else{
+      [  self.rateField setText:[NSString stringWithFormat:@"%.f dogecoin is worth:\n%.6fBTC ",self.amountField.text.floatValue, self.amountField.text.floatValue*returnString.floatValue]];
+      
+    }
+    
+  }
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -60,4 +105,82 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)convertAction:(id)sender {
+  
+  if (self->forward){
+    
+    if(self.BitDollSegControl.selectedSegmentIndex==0){
+      self.typeLabel.text=@"US Dollars";
+      
+    }
+    else self.typeLabel.text=@"Bitcoin";
+  }else self.typeLabel.text=@"Dogecoin";
+  if(self.amountField.text.floatValue==0)return;
+  [self.amountField resignFirstResponder];
+
+  NSString *restCallString;
+  
+  if (self.BitDollSegControl.selectedSegmentIndex == 0){
+    restCallString = [NSString stringWithFormat:@"https://www.dogeapi.com/wow/?a=get_current_price" ];
+  }
+  
+  else{
+    
+    restCallString = [NSString stringWithFormat:@"https://www.dogeapi.com/wow/?a=get_current_price&convert_to=BTC" ];
+    
+  }
+  
+  NSURL *restUrl=[NSURL URLWithString:restCallString];
+  NSURLRequest *restRequest=[NSURLRequest requestWithURL:restUrl];
+  
+  
+  if(self.currentConnection)
+  {
+    [self.currentConnection cancel];
+    self.currentConnection = nil;
+    self.returnData=nil;   }
+  self.currentConnection= [[NSURLConnection alloc]initWithRequest:restRequest delegate:self];
+  self.returnData=[NSMutableData data];
+  
+  
+}
+- (IBAction)directionChangeAction:(id)sender {
+  
+  
+  self->forward=(!self->forward);
+  if (self->forward){
+    
+    if(self.BitDollSegControl.selectedSegmentIndex==0){
+      self.typeLabel.text=@"US Dollars";
+      
+    }
+    else self.typeLabel.text=@"Bitcoin";
+  }else self.typeLabel.text=@"Dogecoin";
+
+
+  UIImage* bimage;
+  if(self->forward){
+    bimage=[UIImage imageNamed:@"forward.png"];
+  }
+  else{
+    bimage=[UIImage imageNamed:@"back.png"];
+  }
+  [((UIButton*)sender) setImage:bimage forState:UIControlStateNormal];
+  
+}
+
+- (IBAction)currChanged:(id)sender {
+  if (self->forward){
+    
+    if(self.BitDollSegControl.selectedSegmentIndex==0){
+      self.typeLabel.text=@"US Dollars";
+      
+    }
+    else self.typeLabel.text=@"Bitcoin";
+  }else self.typeLabel.text=@"Dogecoin";
+}
+-(void)dismissKeyboard{
+  
+  [self.amountField resignFirstResponder];
+}
 @end
